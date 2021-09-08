5b3442fe44f1b7e6e47a50986103834a8d0c908a
ssbzeref <nascentsurya@gmail.com>
2021-09-08 12:44:37 +0545

    script file changed

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -12,7 +12,23 @@
 </div>
 </div>
 
-@include('admin.layouts.scripts')
+{{-- @include('admin.layouts.scripts') --}}
+
+
+<!-- Bootstrap core JavaScript-->
+<script src="{{ asset("assets/admin/vendor/jquery/jquery.min.js") }}"></script>
+<script src="{{ asset("assets/admin/vendor/bootstrap/js/bootstrap.bundle.min.js") }}"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="{{ asset("assets/admin/vendor/jquery-easing/jquery.easing.min.js") }}"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="{{ asset('assets/admin/js/sb-admin-2.min.js') }}"></script>
+
+<script src="{{ mix('js/admin.js') }}"></script>
+
+
+@yield('scripts')
 </body>
 
 </html>
